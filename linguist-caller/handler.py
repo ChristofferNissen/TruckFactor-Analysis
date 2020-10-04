699148262617d74b1cdf596ec7c421c5529f0a29
Christoffer Nissen <christoffer.nissen@gmail.com>
2020-10-04 13:08:06 +0200

    working on linguist

--- linguist-caller/handler.py
+++ linguist-caller/handler.py
@@ -1,17 +1,19 @@
 import subprocess
 import os
+import shutil
 
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
+
     if not os.path.exists("repository/"):
         subprocess.check_output(['git', 'clone', req, 'repository'])
 
     try:
         output = subprocess.check_output(['github-linquist', '--breakdown'], text=True, cwd="repository/")
     finally:
-        os.remove ('repository/')
-        
+        shutil.rmtree("repository/")
+
     return output
